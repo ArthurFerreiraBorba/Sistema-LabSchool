193d293af09a48b8f17ca44e554e074e0c9c5433
Arthur Ferreira Borba <ferreiraborbaarthur@gmail.com>
2024-03-01 16:26:08 -0300

    inicialização projeto

---
+++ src/test/java/br/com/LabSchool/SistemaEscolar/SistemaEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.LabSchool.SistemaEscolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
