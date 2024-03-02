09292361d60514b29ed26c7b1eb54c3556254b08
Arthur Ferreira Borba <ferreiraborbaarthur@gmail.com>
2024-03-02 15:55:38 -0300

    aluno service adicionado

--- src/main/java/br/com/LabSchool/SistemaEscolar/model/ProfessorModel.java
+++ src/main/java/br/com/LabSchool/SistemaEscolar/model/ProfessorModel.java
@@ -2,21 +2,23 @@ package br.com.LabSchool.SistemaEscolar.model;
 
 import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 @Data
-public class Professor {
+public class ProfessorModel {
     
     private static int proximoId = 1;
-    private static ArrayList<Professor> listaProfessores = new ArrayList<>();
+    @Getter private static ArrayList<ProfessorModel> listaProfessores = new ArrayList<>();
 
     @Setter(AccessLevel.NONE) private int id;
     private String nome;
-    private String dataNascimento;
+    private LocalDate dataNascimento;
 
-    public static Professor adicionarProfessor(Professor professor) {
+    public static ProfessorModel adicionarProfessor(ProfessorModel professor) {
         professor.id = proximoId++;
         listaProfessores.add(professor);
         return professor;
---
+++ src/main/java/br/com/LabSchool/SistemaEscolar/service/AlunoService.java
@@ -0,0 +1,30 @@
+package br.com.LabSchool.SistemaEscolar.service;
+
+import br.com.LabSchool.SistemaEscolar.model.AlunoModel;
+import br.com.LabSchool.SistemaEscolar.model.ProfessorModel;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+
+@Service
+public class AlunoService {
+
+    public AlunoModel cadastrarAluno(AlunoModel aluno) throws Exception {
+        if (verificarAluno(aluno)) {
+            return AlunoModel.adicionarAlunos(aluno);
+        }
+        return null;
+    }
+
+    public ArrayList<AlunoModel> verTodosAlunos() {
+        return AlunoModel.getListaAlunos();
+    }
+
+    private static boolean verificarAluno(AlunoModel aluno) throws Exception {
+        if (StringUtils.hasText(aluno.getNome())) {
+            throw new Exception("Nome é obrigatorio");
+        }
+          return true;
+    }
+}
