b4bf2b8ec98197cb47a57c23a4621c2f1fd6accb
Arthur Ferreira Borba <ferreiraborbaarthur@gmail.com>
2024-03-02 16:15:34 -0300

    correção / curso service adicionado

---
+++ src/main/java/br/com/LabSchool/SistemaEscolar/service/CursoService.java
@@ -0,0 +1,29 @@
+package br.com.LabSchool.SistemaEscolar.service;
+
+import br.com.LabSchool.SistemaEscolar.model.CursoModel;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+
+@Service
+public class CursoService {
+
+    public CursoModel cadastrarCurso(CursoModel curso) throws Exception {
+        if (verificarCurso(curso)) {
+            return CursoModel.adicionarCurso(curso);
+        }
+        return null;
+    }
+
+    public ArrayList<CursoModel> verTodosCursos() {
+        return CursoModel.getListaCursos();
+    }
+
+    private static boolean verificarCurso(CursoModel curso) throws Exception {
+        if (StringUtils.hasText(curso.getNome())) {
+            throw new Exception("Nome é obrigatorio");
+        }
+          return true;
+    }
+}
